9bd3a3ae277fe2e22577750c457b11fe0ce08d24
Rohith <rohithuppalapati77@gmail.com>
2021-08-15 22:10:10 +0530

    email sending with html file

--- Email/sending_mail.py
+++ Email/sending_mail.py
@@ -1,5 +1,9 @@
 from email.message import EmailMessage
 import smtplib
+from pathlib import Path
+from string import Template
+
+#path=Template(Path('index.html').read_text())
 
 email=EmailMessage()
 email['from']='davidoff'
@@ -7,6 +11,7 @@ email['to']='rohithuppalapati77@gmail.com'
 email['subject']='Our family reunion'
 
 email.set_content('hello greetings from davidoff')
+#email.set_content(path.substitute({'name':'Rohith'}),'html')
 
 server=smtplib.SMTP(host='smtp.gmail.com',port=587)
 server.ehlo()
@@ -15,6 +20,8 @@ server.login('david7330654748@gmail.com','rohith1414')
 server.send_message(email)
 
 print('mail sent !!!!!')
+
+
 
 
 
